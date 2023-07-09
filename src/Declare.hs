ceec2a5de99a18a1c1161b8e24e00f2e805fc979
Matt <mwpryer@gmail.com>
2023-07-10 02:42:19 +0800

    Add variable declarations

--- src/Declare.hs
+++ src/Declare.hs
@@ -1,5 +1,6 @@
 module Declare where
 
+-- Abstract syntax of expressions in the language
 data Exp
   = Num Int
   | Add Exp Exp
@@ -8,4 +9,12 @@ data Exp
   | Div Exp Exp
   | Pow Exp Exp
   | Neg Exp
+  | Var String
+  | Decl String Exp Exp
   deriving (Show)
+
+-- Represents association of a variable with its value
+type Binding = (String, Int)
+
+-- Execution environment for mapping variables to values
+type Env = [Binding]
